c7f19bb3148ec8b45fcf813d469678c238b32036
Elias Berkowitz <eliberkowitz@gmail.com>
2019-05-01 11:18:34 -0400

    course templating init, build script pretty much done

--- build.py
+++ build.py
@@ -1,35 +1,72 @@
-from jinja2 import Environment, FileSystemLoader
+import random
+import csv
+from jinja2 import Environment, FileSystemLoader, exceptions
 from dataclasses import dataclass
-from typing import List
+from typing import List, Dict
+from collections import defaultdict
 
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('index.jhtml')
 
+arms = ["💪"]  # #  , "💪🏿", "💪🏼", "💪🏽", "💪🏾"]
+
 
 @dataclass
 class Course:
-    code: str = 'Place'
-    critical_review: str = 'hi'
-    old_syllabus: str = 'hi'
-    hours: float = 3.5
-    difficulty: int = 2
-    notes: str = 'hello there'
+    code: str
+    critical_review: str
+    old_syllabus: str
+    hours: str
+    difficulty: int
+    notes: str
+
+    def __post_init__(self):
+        self.difficulty = [random.choice(arms) for _ in range(self.difficulty)]
+        min_name = self.code.replace(' ', '').lower()
+        self.link = f"courses/{min_name}.html"
+        self.template_link = f"templates/{min_name}.jhtml"
+        try:
+            self.template = env.get_template(f'{min_name}.jhtml')
+        except exceptions.TemplateNotFound:
+            pass
 
 
-@dataclass
-class Topic:
-    description: str
-    courses: List[Course]
+topics: Dict[str, List[Course]] = defaultdict(list)
+for line in csv.DictReader(open('courses.csv')):
+    course = Course(line['Course'], line['CriticalReview'],
+                    line['Syllabus'], line['Hours'],
+                    int(line['Difficulty']), line['Notes'])
+    topics[line['Topic']].append(course)
 
 
-calc1 = Topic('Intro calc', [Course(), Course(), Course()])
+def compile():
+    rendered_template = template.render(topics=topics)
 
-topics = [calc1, calc1]
+    with open('index.html', 'w') as f:
+        f.write(rendered_template)
 
-rendered_template = template.render(topics=topics)
+    for topic in topics:
+        for course in topics[topic]:
+            t = course.template.render(course=course)
+            with open(course.link, 'w') as f:
+                f.write(t)
 
-with open('index.html', 'w') as f:
-    f.write(rendered_template)
+    print('recompiled')
 
 
-print('recompiled')
+def write_empty_files():
+    # assert False, 'u sure tho'
+    base_template = open('templates/base.html').read()
+    for topic in topics:
+        for course in topics[topic]:
+            with open(course.template_link, 'w') as f:
+                f.write(base_template)
+
+            course.__post_init__()
+
+    print('written')
+
+
+if __name__ == '__main__':
+    # write_empty_files()
+    compile()
